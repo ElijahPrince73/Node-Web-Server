87ad5e00560b319222094bdb376aa099c8eb018f
Elijah Prince <elijahprince73@gmail.com>
2018-01-30 20:19:32 -0800

    Express Middleware

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 const app = express()
 
 hbs.registerPartials(__dirname + '/partials')
 app.set('view engine', hbs)
+
+app.use((req, res, next) => {
+	var now = new Date().toString()
+	var log = `Date:${now}; Method: ${req.method}; URL:${req.url}`
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append file to server.log');
+		}
+	})
+	next()
+})
+
+// app.use((req, res, next) => {
+// 	res.render('maintance.hbs')
+// })
+
+app.use(express.static(__dirname + '/public'))
+
 
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear()

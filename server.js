d41fe6b2a95b4796e03a5a0d68981a0f3241647b
Elijah Prince <elijahprince73@gmail.com>
2018-02-03 17:16:14 -0800

    Can use different server port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000
+
 const app = express()
 
 hbs.registerPartials(__dirname + '/partials')
@@ -51,6 +54,6 @@ app.get('/bad', (req, res) => {
 		error: "Unable to find that page"
 	})
 })
-app.listen(3000, () => {
-	console.log('Server is up on port 3000');
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 })
